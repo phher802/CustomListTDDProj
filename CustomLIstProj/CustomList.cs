a4b453123af20e0612a4395857caf314f3f94496
phher802 <your_email@example.com>
2020-11-23 13:56:02 -0600

    Create test method to add integers with count of 3 and capacity of 5

--- CustomLIstProj/CustomList.cs
+++ CustomLIstProj/CustomList.cs
@@ -4,9 +4,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CustomLIstProj
+namespace CustomListProj
 {
-    class CustomList
+    public class CustomList<T>
     {
+        // member variables
+        T[] _items;
+        public int Count;
+        public int Capacity;
+
+        //constructor
+        public CustomList()
+        {
+            Count = 0;
+            Capacity = 4;
+            _items = new T[Capacity];
+        }
+
+        // methods
+        public void Add(T valueToAdd)
+        {
+            _items[Count] = valueToAdd;
+            Count++;
+        }
+        
     }
+
 }
+
--- UnitTestCustomListProj/CustomListTest.cs
+++ UnitTestCustomListProj/CustomListTest.cs
@@ -8,8 +8,43 @@ namespace UnitTestCustomListProj
     public class CustomListTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Add_PostiveInteger_CountOfOne()
         {
+            //Arrange - variables, objects, etc.
+            CustomList<int> myList = new CustomList<int>();
+            int value1 = 5;
+            int expected = 1;
+            int actual;
+
+
+            //Act
+            myList.Add(value1);
+            actual = myList.Count;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Add_PostiveInteger_CountOf3()
+        {
+            //Arrange - variables, objects, etc.
+            CustomList<int> myList = new CustomList<int>();
+            int value1 = 5;
+            int value2 = 8;
+            int value3 = 9;
+            int expected = 1;
+            int actual;
+
+
+            //Act
+            myList.Add(value1);
+            myList.Add(value2);
+            myList.Add(value3);
+            actual = myList.Count;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
         }
     }
 }
