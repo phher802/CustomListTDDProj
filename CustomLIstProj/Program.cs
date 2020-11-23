a2d4efc36aea3e7d28f593fcad74786868a24469
phher802 <your_email@example.com>
2020-11-23 16:14:13 -0600

    Changed add string to list test method to expect capacity of 8

--- CustomLIstProj/Program.cs
+++ CustomLIstProj/Program.cs
@@ -10,6 +10,8 @@ namespace CustomListProj
     {
         static void Main(string[] args)
         {
+            //CustomList<string> customList = new CustomList<string>;
+            
         }
     }
 }
