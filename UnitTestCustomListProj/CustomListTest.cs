f0988b9064776e7cbffab213cd2e69841a2ce39e
phher802 <your_email@example.com>
2020-11-23 12:47:06 -0600

    Init

---
+++ UnitTestCustomListProj/CustomListTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using CustomListProj;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestCustomListProj
+{
+    [TestClass]
+    public class CustomListTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ UnitTestCustomListProj/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestCustomListProj")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestCustomListProj")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5b2647e3-eea5-462a-b5e0-182e1d2da1ed")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
