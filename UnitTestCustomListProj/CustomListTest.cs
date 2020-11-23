8d0a9fcd12a9c0bdd482f35b2944bd69d5695b5f
phher802 <pher024@gmail.com>
2020-11-23 14:12:42 -0600

    Create test to add a list of string and display string at index 4

--- UnitTestCustomListProj/CustomListTest.cs
+++ UnitTestCustomListProj/CustomListTest.cs
@@ -33,7 +33,7 @@ namespace UnitTestCustomListProj
             int value1 = 5;
             int value2 = 8;
             int value3 = 9;
-            int expected = 1;
+            int expected = 3;
             int actual;
 
 
@@ -42,6 +42,60 @@ namespace UnitTestCustomListProj
             myList.Add(value2);
             myList.Add(value3);
             actual = myList.Count;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void Add_PostiveInteger_CapacityOf5()
+        {
+            //Arrange - variables, objects, etc.
+            CustomList<int> myList = new CustomList<int>();
+            int value1 = 5;
+            int value2 = 8;
+            int value3 = 9;
+            int value4 = 9;
+            int value5 = 9;
+            int expected = 5;
+            int actual;
+
+
+            //Act
+            myList.Add(value1);
+            myList.Add(value2);
+            myList.Add(value3);
+            myList.Add(value4);
+            myList.Add(value5);
+            actual = myList.Capacity;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Add_StringToList_CountOf6()
+        {
+            //Arrange - variables, objects, etc.
+            CustomList<string> myList = new CustomList<string>();
+            string hello = "Hello";
+            string comma = ", ";
+            string how = "How";
+            string are = "are";
+            string you  = "you";
+            string quetionMark = "? ";
+
+            string expected = "are";
+            string actual;
+
+
+            //Act
+            myList.Add(hello);
+            myList.Add(comma);
+            myList.Add(how);
+            myList.Add(are);
+            myList.Add(you);
+            myList.Add(quetionMark);
+            actual = myList.Count.ToString();
 
             //Assert
             Assert.AreEqual(expected, actual);
