2107426c5f487cb1c999c94725fafb84ef74fb3d
phher802 <pher024@gmail.com>
2020-11-24 09:10:21 -0600

    All remove tests passed

--- CustomLIstProj/Program.cs
+++ CustomLIstProj/Program.cs
@@ -12,7 +12,6 @@ namespace CustomListProj
         {
             CustomList<string> customList = new CustomList<string>();
 
-            CustomListProj customList = new CustomListProj();
 
         }
     }
